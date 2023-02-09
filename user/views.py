e0e0f0b9ea43fb629ca6ec05dad581652f653ac5
willcutrim <willyam894@gmail.com>
2023-02-09 20:32:42 -0300

    Feat: adicionada a funcionalide de login ddo usuario

--- user/views.py
+++ user/views.py
@@ -1,5 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import status
+from user.serializers import UserSerializer
 
-def index(request):
-    return HttpResponse('Index user')
+class UserApiView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            response = {
+                'message': 'Login successful',
+                'user': {
+                    'id': str(user.id),
+                    'username': str(user.username),
+                    'email': str(user.email),
+                }
+            }
+            return Response(data=response, status=status.HTTP_200_OK)
+        else:
+            return Response(data={'message': 'Usuário ou senha inválida.'}, status=status.HTTP_400_BAD_REQUEST)
